bf9858eee1456ae8a30d0ada3cda3f386c706161
gmohmad <germihanovmohmad@gmail.com>
2026-03-28 22:43:53 +0300

    remove frontend

--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import '@photo-sphere-viewer/core/index.css';
-import '@photo-sphere-viewer/virtual-tour-plugin/index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<App />
-);
